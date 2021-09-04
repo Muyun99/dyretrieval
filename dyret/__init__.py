e0c94cbef4848d7c9db1751cda39a50b7ec0831c
muyun99 <27944418+Muyun99@users.noreply.github.com>
2021-09-04 23:29:24 +0800

    [update] update utils

--- dyret/__init__.py
+++ dyret/__init__.py
@@ -1,7 +1,7 @@
 from .models import build_model, build_loss
-
+from .samplers import build_sampler
 from .optimizers import build_optimizer
 from .schedulers import build_scheduler
 from .datasets import build_dataset, build_dataloader
 
-__all__ = ['build_model', 'build_optimizer', 'build_loss', 'build_scheduler', 'build_dataset', 'build_dataloader']
+__all__ = ['build_model', 'build_optimizer', 'build_loss', 'build_scheduler', 'build_dataset', 'build_dataloader', 'build_sampler']
